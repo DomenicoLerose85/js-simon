d29e46833358e81bec39ebff3e6ce5d0aa58482f
DomenicoLerose85 <domenicolerose85@libero.it>
2022-04-06 16:17:57 +0200

    Js_started

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function generatorRandomNumber(min, max) {
-    range = max - min + 1;
-    return Math.floor(Math.random() * range) + min;
-}
 
 // Visualizzare in pagina 5 numeri casuali (in un range tra 1 e 100). 
 // Da lì parte un timer di 30 secondi.
@@ -9,3 +5,33 @@ function generatorRandomNumber(min, max) {
 // Dopo che sono stati inseriti i 5 numeri, il software dice 
 // quanti e quali dei numeri da indovinare sono stati individuati.
 
+// generatore numeri random
+
+function generatorRandomNumber(min, max) {
+    range = max - min + 1;
+    return Math.floor(Math.random() * range) + min;
+}
+
+// arrays per numeri creati dalla macchina e numeri inseriti dall'utente
+
+let machineSequence = [];
+let humanSequence = [];
+
+// selezione start btn e funzione per consentire inizio gioco quando il btn viene cliccato
+
+const startBtn = document.getElementById("start_btn");
+
+function startGame() {
+    startBtn.classList.add('hidden');
+}
+  
+startBtn.addEventListener('click', startGame);
+
+// funzione per generare i numeri random da 1 a 100
+function numberGeneration() {
+    const randomNumbers = numberGeneration[Math.floor(Math.random() * 100)];
+    console.log(randomNumbers);
+    return randomNumbers;
+}
+
+document.writeln 
