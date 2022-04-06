3b7089a036c150b32438f19a7147de6deeeaf562
DomenicoLerose85 <domenicolerose85@libero.it>
2022-04-06 15:36:57 +0200

    html_&_css_static_done

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function generatorRandomNumber(min, max) {
     range = max - min + 1;
     return Math.floor(Math.random() * range) + min;
 }
+
+// Visualizzare in pagina 5 numeri casuali (in un range tra 1 e 100). 
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice 
+// quanti e quali dei numeri da indovinare sono stati individuati.
+
